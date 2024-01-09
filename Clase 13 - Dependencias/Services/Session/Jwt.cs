0556c89a17a1aa29a5dda6b2f9aa59941ffe8efc
Fabian Lucena <fabianlucena@gmail.com>
2024-01-09 10:07:33 -0300

    Clase 13: Formas de cargar las dependencias.

---
+++ Clase 13 - Dependencias/Services/Session/Jwt.cs
@@ -0,0 +1,11 @@
+﻿namespace Logic.Session
+{
+    public class Jwt
+    {
+        public string Key { get; set; } = string.Empty;
+        public string Issuer { get; set; } = string.Empty;
+        public string Audience { get; set; } = string.Empty;
+        public string Subject { get; set; } = string.Empty;
+        public TimeSpan ValidityTime { get; set; } = TimeSpan.FromSeconds(300);
+    }
+}
---
+++ Clase 13 - Dependencias/Services/Session/LoginRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace Logic.Session
+{
+    public class LoginRequest
+    {
+        public string Username { get; set; } = "";
+        public string Password { get; set; } = "";
+    }
+}
